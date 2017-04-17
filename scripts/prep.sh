d8b9e4e4692777c6867d482aa737054c12593521
E.J. Bevenour <ejbevenour@live.com>
2017-04-16 20:47:29 -0400

    added skeliton

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,4 +1,13 @@
 curl -O https://runtime.fivem.net/client/cfx-server.7z
 p7zip -d cfx-server.7z
 rm -rf cfx-server.7z
+pushd cfx-server
+#TODO add CI mod to AutostartResources in citmp.yml
+#TODO copy CI mod to resources folder
+
+#TODO 
+#if CI_STATUS line one equals fail then
 exit 1
+#else
+#if  CI_STATUS line one equals pass then
+# exit 0
