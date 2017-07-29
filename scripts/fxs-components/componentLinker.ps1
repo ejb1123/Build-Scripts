44a76bb3fd66e4040bd849b0cada1dcc82da1718
ejb1123 <address>
2017-07-29 21:53:41 +0200

    updated

--- scripts/fxs-components/componentLinker.ps1
+++ scripts/fxs-components/componentLinker.ps1
@@ -3,8 +3,9 @@
 [string]$WorkDir = "C:\fivem\dev\fivem"
 )
 Push-Location -Path "$SrcDir\components"
-[System.Collections.ArrayList] $col = get-content -Path "$WorkDir\data\server_windows\components.json" | ConvertFrom-Json
+[System.Collections.Generic.List[string]] $col = get-content -Path "$WorkDir\data\server_windows\components.json" | ConvertFrom-Json
 $col.Remove("svadhesive")
+$col
 Get-ChildItem -Path "." | foreach {
     $fullname = $_.FullName
     if($(Test-Path "$fullname\component.json") -eq $true){
@@ -18,13 +19,13 @@ Get-ChildItem -Path "." | foreach {
             }
             else{
                 Add-Content -Value "component `'fs-web`'`n" -Path "$WorkDir\code\components\config.lua" -Encoding UTF8
-                $col.Add("$j.name")
+                $col.Add($j.name)
             }
             if(! $(Test-Path -PathType Container "$WorkDir\code\components\$l") -or $(Get-ItemPropertyValue -Path "$WorkDir\code\components\$ll" -Name "LinkType" ) -ne "Junction"){
                 New-Item -Path "$WorkDir\code\components" -ItemType Junction -Name $ll -Value "$fullname" -Force
             }
         }
     }
-    Set-Content -Path "$WorkDir\data\server_windows\components.json" -Value $(ConvertTo-Json("$col"))
+    Set-Content -Path "$WorkDir\data\server_windows\components.json" -Value $($(ConvertTo-Json($col)).Replace('[','}').Replace(']','}'))
     Pop-Location
 }
